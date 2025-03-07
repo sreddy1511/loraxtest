641ad9de7bdf5c8a6e5021bee1f4099a0af761ce
sreddy1511 <sreddy1511@gmail.com>
2025-03-07 19:59:43 +0000

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            return a / b;
+        }
+    }
+}
