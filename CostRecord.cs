a1b0424d61980d8e9ca4a380b2aa5117813b33e1
sreddy1511 <sreddy1511@gmail.com>
2025-03-07 19:51:17 +0000

    Create CostRecord.cs

---
+++ CostRecord.cs
@@ -0,0 +1,13 @@
+namespace JsonTests
+{
+    public class CostRecord
+    {
+        public string YearId { get; set; }
+        public int GeoRegionId { get; set; }
+        public int CountryId { get; set; }
+        public int RegionId { get; set; }
+        public int SchemeId { get; set; }
+        public int SchmTypeId { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
